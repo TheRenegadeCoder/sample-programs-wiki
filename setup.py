fd2412d90785629847563f814b64fa5ab37a64e5
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-11 00:35:55 -0400

    Fixed up setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='generate_wiki',
-    version='1.0.0',
-    scripts=['generate_wiki']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="generate_wiki",
+    version="1.0.6",
+    author="The Renegade Coder",
+    author_email="jeremy.grifski@therenegadecoder.com",
+    description="A wiki generation package for the Sample Programs repo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheRenegadeCoder/sample-programs-wiki-generator",
+    entry_points = {
+        "console_scripts": [
+            'generate_wiki = generate_wiki:main',
+        ]
+    },
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
 )
