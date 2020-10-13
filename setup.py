3c7188dd2f19ffdacb15a3287b324ac3a88069ba
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-10-13 12:21:47 -0400

    Added Test Column Support (#28)
    * Added Test Column Support
    
    * Incremented Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="generate_wiki",
-    version="1.1.2",
+    version="1.2.0",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="A wiki generation package for the Sample Programs repo",
