40ff5d27fd884742b6dcf4dad313ec7b0b65a65e
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-05-20 11:53:43 -0400

    Beginning to toy with readme generation

--- generate_wiki/readme.py
+++ generate_wiki/readme.py
@@ -1,6 +1,11 @@
+from generate_wiki.markdown import MarkdownPage
 from generate_wiki.repo import Repo
 
 
 class ReadMeCatalog:
     def __init__(self, repo):
         self.repo: Repo = repo
+        self.pages: list[MarkdownPage] = list()
+
+    def build_readme(self, page: MarkdownPage):
+        pass
