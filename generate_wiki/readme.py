d3e6dbd1bc5edef74ecf61f3fed0c7cd9dcbb169
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-05-20 19:54:49 -0400

    Updated README format

--- generate_wiki/readme.py
+++ generate_wiki/readme.py
@@ -6,19 +6,11 @@ def _get_intro_text(language: LanguageCollection) -> str:
     # TODO: need to think about languages that don't exist in documentation
     return f"""Welcome to Sample Programs in {language.name.capitalize()}! To find 
     documentation related to the {language.name.capitalize()} code in this repo, look 
-    [here]({language.sample_program_url})."""
-
-
-def _get_sample_programs_text() -> str:
-    """
-    Produces the sample programs boilerplate text about the list of code snippets to follow.
-    :return: sample programs boilerplate
-    """
-    return """Below, you'll find a list of code snippets in this collection.
-    Code snippets preceded by :warning: link to an article request 
-    issue while code snippets preceded by :white_check_mark: link
-    to an existing article which provides further documentation.
-    """
+    [here]({language.sample_program_url}). Otherwise, below you'll find a list of code 
+    snippets in this collection. Code snippets preceded by :warning: link to a GitHub 
+    issue query featuring a possible article request issue. If an article request issue 
+    doesn't exist, we encourage you to create one. Meanwhile, code snippets preceded 
+    by :white_check_mark: link to an existing article which provides further documentation."""
 
 
 def _generate_program_list(language: LanguageCollection) -> list:
@@ -59,10 +51,6 @@ class ReadMeCatalog:
         page.add_content(f"# Sample Programs in {language.name.capitalize()}")
         page.add_section_break()
         page.add_content(_get_intro_text(language))
-        page.add_section_break()
-        page.add_content("## Sample Programs")
-        page.add_section_break()
-        page.add_content(_get_sample_programs_text())
         page.add_section_break()
         page.add_content(*_generate_program_list(language))
         page.add_section_break()
