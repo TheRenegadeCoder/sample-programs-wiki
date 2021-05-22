a6bafa180920ee6c902d4e8cea26c739e7371667
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-05-22 12:30:24 -0400

    Updated setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="generate_docs",
-    version="2.1.0",
+    version="2.2.0",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="A docs generation package for the Sample Programs repo",
