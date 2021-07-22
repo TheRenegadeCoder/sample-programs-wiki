e61d98c6ac37b3ded007467af6ea29457c597e0c
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-21 23:30:11 -0400

    Slowly updating code to use InlineText

--- generate_docs/wiki.py
+++ generate_docs/wiki.py
@@ -1,6 +1,6 @@
 import os
 
-from snakemd import Document, InlineText, Paragraph
+from snakemd import Document, InlineText, Paragraph, Table
 
 from generate_docs.repo import Repo, LanguageCollection
 
@@ -23,59 +23,64 @@ class Wiki:
         self._build_alphabet_catalog()
         self._build_alphabet_pages()
 
-    def _build_repo_link(self, text: str, letter: str, language: str) -> str:
+    def _build_repo_link(self, text: str, letter: str, language: str) -> InlineText:
         """
         A helper method which creates a link to the language folder in the repo
         (e.g., https://github.com/TheRenegadeCoder/sample-programs/tree/main/archive/c/c-sharp.)
-        :param text: the link text
-        :param letter: the starting letter of the language
-        :param language: the language to link
+
+        :param str text: the link text
+        :param str letter: the starting letter of the language
+        :param str language: the language to link
         :return: a markdown link to a sample programs language page
         """
-        return str(InlineText(text, url=f"{self.repo_url_base}{letter}/{language}"))
+        return InlineText(text, url=f"{self.repo_url_base}{letter}/{language}")
 
-    def _build_issue_link(self, language: str) -> str:
+    def _build_issue_link(self, language: str) -> InlineText:
         """
         A helper method which creates a link to all issues matching that language.
-        :param language: the language to search for issues
+
+        :param str language: the language to search for issues
         :return: a markdown link to a GitHub query for issues matching this language
         """
         lang_query = language.replace("-", "+")
-        return str(InlineText("Here", f"{self.issue_url_base}{lang_query}"))
+        return InlineText("Here", f"{self.issue_url_base}{lang_query}")
 
-    def _build_test_link(self, language: LanguageCollection, letter: str) -> str:
+    def _build_test_link(self, language: LanguageCollection, letter: str) -> InlineText:
         """
         A helper method which creates a link to the test file for a given language collection.
-        :param language: a language collection
-        :param letter: the first letter of the language
+
+        :param LanguageCollection language: a language collection
+        :param str letter: the first letter of the language
         :return: a markdown link to a test file if it exists; an empty string otherwise
         """
         test_file_path = language.test_file_path
         if test_file_path:
             file_path = self.repo_url_base + letter + "/" + language.name + "/" + os.path.basename(test_file_path)
-            file_path_link = str(InlineText("Here", url=file_path))
+            file_path_link = InlineText("Here", url=file_path)
         else:
-            file_path_link = ""
+            file_path_link = InlineText("")
         return file_path_link
 
     @staticmethod
-    def _build_language_link(language: LanguageCollection) -> str:
+    def _build_language_link(language: LanguageCollection) -> InlineText:
         """
         A handy abstraction for the create_md_link() method which creates a link to a sample programs language page.
         (e.g., https://sample-programs.therenegadecoder.com/languages/c/)
-        :param language: the language to link
+
+        :param LanguageCollection language: the language to link
         :return: a markdown link to the language page if it exists; an empty string otherwise
         """
         lang = InlineText("Here", language.sample_program_url)
         if not lang.verify_url():
             lang = InlineText("")
-        return str(lang)
+        return lang
 
-    def _build_alphabet_page(self, letter: str):
+    def _build_alphabet_page(self, letter: str) -> Document:
         """
         A helper method which generates a single wiki alphabet page given a letter.
-        :param letter: the starting letter of a set of programming languages (e.g., "p" for [pascal, perl, python])
-        :return:
+
+        :param str letter: the starting letter of a set of programming languages (e.g., "p" for [pascal, perl, python])
+        :return: a alphabet page markdown document
         """
         page = Document(letter.capitalize())
         page.add_paragraph(
@@ -96,12 +101,13 @@ class Wiki:
             tests_link = self._build_test_link(language, letter)
             body.append([language_link, tag_link, issues_link, tests_link, str(language.total_snippets)])
         body.append(["**Totals**", "", "", "", str(total_snippets)])
-        page.add_table(header, body)
+        page.add_element(Table(header, body))
         return page
 
     def _build_alphabet_pages(self) -> None:
         """
         Builds a set of wiki alphabet pages from the repo.
+        
         :return: None
         """
         alphabetical_list = self.repo.get_sorted_language_letters()
