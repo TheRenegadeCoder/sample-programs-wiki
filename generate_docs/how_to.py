5291aedd7610722b7eff0096ff9cc9c2a300e1db
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-21 23:45:15 -0400

    Reworked intro text

--- generate_docs/how_to.py
+++ generate_docs/how_to.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 
 import feedparser
@@ -5,13 +6,14 @@ import requests
 from bs4 import BeautifulSoup
 from snakemd import Document, Table, InlineText
 
+logger = logging.getLogger(__name__)
 
-def get_intro_text():
-    return """Welcome to a collection of Jupyter Notebooks from the 
-    [How to Python](https://therenegadecoder.com/series/how-to-python/) series on The Renegade Coder. For convenience, 
-    you can access all of the articles, videos, challenges, and source code below. Alternatively, I keep 
-    [an enormous article](https://therenegadecoder.com/code/python-code-snippets-for-everyday-problems/) up to date 
-    with all these snippets as well.
+
+def get_intro_text() -> str:
+    return """
+    Welcome to a collection of Jupyter Notebooks from the How to Python series on The Renegade Coder. For 
+    convenience, you can access all of the articles, videos, challenges, and source code below. Alternatively, I keep 
+    an enormous article up to date with all these snippets as well.
     """
 
 
@@ -47,7 +49,7 @@ def get_slug(title: str, sep: str):
 def get_challenge(title: str):
     slug = get_slug(title, "-")
     base = "https://github.com/TheRenegadeCoder/how-to-python-code/tree/main/challenges/"
-    print(f"Trying {base}{slug}")
+    logger.debug(f"Trying {base}{slug}")
     request = requests.get(f"{base}{slug}")
     if request.status_code == 200:
         return f"{base}{slug}"
@@ -56,7 +58,7 @@ def get_challenge(title: str):
 def get_notebook(title: str):
     slug = get_slug(title, "_")
     base = "https://github.com/TheRenegadeCoder/how-to-python-code/tree/main/notebooks/"
-    print(f"Trying {base}{slug}.ipynb")
+    logger.debug(f"Trying {base}{slug}.ipynb")
     request = requests.get(f"{base}{slug}.ipynb")
     if request.status_code == 200:
         return f"{base}{slug}.ipynb"
@@ -65,7 +67,7 @@ def get_notebook(title: str):
 def get_test(title: str):
     slug = get_slug(title, "_")
     base = "https://github.com/TheRenegadeCoder/how-to-python-code/tree/main/testing/"
-    print(f"Trying {base}{slug}.py")
+    logger.debug(f"Trying {base}{slug}.py")
     request = requests.get(f"{base}{slug}.py")
     if request.status_code == 200:
         return f"{base}{slug}.py"
@@ -86,7 +88,12 @@ class HowTo:
 
         # Introduction
         self.page.add_header("How to Python - Source Code")
-        self.page.add_paragraph(get_intro_text())
+        self.page.add_paragraph(get_intro_text()) \
+            .insert_link("How to Python", "https://therenegadecoder.com/series/how-to-python/") \
+            .insert_link(
+                "an enormous article",
+                "https://therenegadecoder.com/code/python-code-snippets-for-everyday-problems/"
+            )
 
         # Table
         headers = [
