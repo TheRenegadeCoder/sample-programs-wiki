783ec7dc07af22c372832b41448638db799ae4e4
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-22 17:49:09 -0400

    Updated link replacement for READMEs

--- generate_docs/readme.py
+++ generate_docs/readme.py
@@ -36,7 +36,7 @@ def _generate_program_list(language: LanguageCollection) -> MDList:
             .insert_link("Requirements", program.sample_program_req_url)
         if not program_line.verify_urls()[program.sample_program_doc_url]:
             program_line.replace(":white_check_mark:", ":warning:")
-            program_line.insert_link(program_name, program.sample_program_issue_url)
+            program_line.replace_link(program_name, program.sample_program_issue_url)
         list_items.append(program_line)
     return MDList(list_items)
 
