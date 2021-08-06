d08716185b0ca0272e7c27c3a1cf7af4589f8ace
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-06 18:44:38 -0400

    Updated setup file

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "beautifulsoup4~=4.9.3",
         "requests~=2.25.1",
         "SnakeMD~=0.7.0",
-        "subete~=0.3.1"
+        "subete~=0.4.0"
     ],
     entry_points={
         "console_scripts": [
