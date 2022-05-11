f9f1b2eb072084bdece11bef0b7712778b2bcc60
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-05-11 00:25:37 -0400

    Upgraded library requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jisho",
-    version="0.1.2",
+    version="0.1.3",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
     description="A wiki generation package for the Sample Programs repo",
@@ -14,8 +14,8 @@ setuptools.setup(
     url="https://github.com/TheRenegadeCoder/sample-programs-wiki",
     packages=setuptools.find_packages(),
     install_requires=[
-        "SnakeMD>=0.7",
-        "subete>=0.9"
+        "SnakeMD>=0.10",
+        "subete>=0.11"
     ],
     entry_points={
         "console_scripts": [
